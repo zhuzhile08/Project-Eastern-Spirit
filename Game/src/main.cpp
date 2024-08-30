8ea4e04c3f8d13e564801a4292f6a16ee0f376ab
Zhile Zhu <zhuzhile08@gmail.com>
2024-08-30 20:45:37 +0200

    Add basic camera code and improvements

--- Game/src/main.cpp
+++ Game/src/main.cpp
@@ -11,10 +11,8 @@
 #include <random>
 #include <map>
 
-static constexpr std::size_t windowWidth = 320;
-static constexpr std::size_t windowHeight = 240;
-static constexpr std::size_t windowScale = 2;
 static constexpr std::size_t fov = 90;
+static const float fovRatio = glm::tan(glm::radians(static_cast<float>(fov / 2)));
 
 namespace {
 
@@ -32,6 +30,8 @@ struct DrawData {
 int main(int argc, char** args) {
 	bool running = true;
 
+	glm::ivec2 windowDim(1280, 720);
+
 	float fps = 1;
 	float deltaTime = 0;
 	std::uint64_t frameStart = 0;
@@ -46,8 +46,6 @@ int main(int argc, char** args) {
 
 	SDL_Texture* textureAtlas = { };
 	glm::ivec3 textureDim = { }; // z represents the channels
-
-	float fovRatio = glm::tan(glm::radians(static_cast<float>(fov / 2)));
 
 	std::map<int, lsd::Vector<DrawData>, std::greater<float>> drawData = { };
 
@@ -64,8 +62,8 @@ int main(int argc, char** args) {
 
 	if (SDL_CreateWindowAndRenderer(
 		"車万朱元璋", 
-		windowWidth * windowScale, windowHeight * windowScale, 
-		SDL_WINDOW_RESIZABLE | SDL_WINDOW_RESIZABLE, &window, &renderer) == SDL_FALSE) {
+		windowDim.x, windowDim.y, 
+		SDL_WINDOW_RESIZABLE, &window, &renderer) == SDL_FALSE) {
 		std::printf("Failed to create SDL Window and/or Renderer with error: \"%s\"", SDL_GetError());
 		return 0;
 	}
@@ -90,42 +88,53 @@ int main(int argc, char** args) {
 	stbi_image_free(pixels);
 
 
-	std::uniform_int_distribution<int> randXGen(-50, 50);
+	auto camera = etcs::insertEntity();
+	camera.insertComponent<etcs::Transform>();
+
+	auto camTransform = camera.component<etcs::Transform>();
+
+
+	std::uniform_int_distribution<int> randXGen(-20, 20);
 	std::uniform_int_distribution<std::size_t> randYGen(0, 64);
-	std::uniform_int_distribution<int> randZGen(-50, 50);
+	std::uniform_int_distribution<int> randZGen(-20, 20);
 
 	for (size_t i = 2049; i > 1; i--) {
 		auto e = etcs::insertEntity();
 
 		e.insertComponent<etcs::Transform>(glm::vec3(randXGen(randEngine), randYGen(randEngine), randZGen(randEngine)));
 		//e.insertComponent<etcs::Transform>(glm::vec3(1, 10, 1));
-		e.insertComponent<SpriteRect>();
+		auto rect = e.insertComponent<SpriteRect>();
 
-		if (i % 17 == 0) e.component<SpriteRect>().rect = { 0, 0, 16, 16 };
-		if (i % 13 == 0) e.component<SpriteRect>().rect = { 16, 0, 16, 16 };
-		if (i % 11 == 0) e.component<SpriteRect>().rect = { 32, 0, 32, 32 };
-		if (i % 7 == 0)	e.component<SpriteRect>().rect = { 0, 16, 16, 16 };
-		if (i % 5 == 0) e.component<SpriteRect>().rect = { 16, 16, 16, 16 };
-		if (i % 3 == 0)	e.component<SpriteRect>().rect = { 0, 32, 32, 32 };
-		if (i % 2 == 0) e.component<SpriteRect>().rect = { 32, 32, 32, 32 };
+		if (i % 17 == 0) rect.get() = { 0, 0, 16, 16 };
+		if (i % 13 == 0) rect.get() = { 16, 0, 16, 16 };
+		if (i % 11 == 0) rect.get() = { 32, 0, 32, 32 };
+		if (i % 7 == 0)	rect.get() = { 0, 16, 16, 16 };
+		if (i % 5 == 0) rect.get() = { 16, 16, 16, 16 };
+		if (i % 3 == 0)	rect.get() = { 0, 32, 32, 32 };
+		if (i % 2 == 0) rect.get() = { 32, 32, 32, 32 };
 	}
 
 
 	auto renderSystem = etcs::insertSystem<const etcs::Transform, const SpriteRect>();
+
 	lsd::Function<void(const etcs::Transform&, const SpriteRect&)> renderFunction = 
-		[&drawData, &deltaTime, fovRatio, renderer, textureAtlas](const etcs::Transform& transform, const SpriteRect& rect) {
-		float fovRatY = transform.translation.y * fovRatio;
+		[&camTransform, &windowDim, &drawData, &deltaTime, renderer, textureAtlas](const etcs::Transform& transform, const SpriteRect& rect) {
+			if (transform.translation().y > 0) {
+				auto translation = camTransform.get().localTransform() * glm::vec4(transform.translation(), 0.0f);
+				
+				float fovRatY = translation.y * fovRatio;
 
-		float px = transform.translation.x / fovRatY * windowWidth + windowWidth / 2; // perspective calculations
-		float py = -transform.translation.z / fovRatY * windowHeight + windowHeight / 2;
-		float w = windowWidth / fovRatY;
+				float px = translation.x / fovRatY * windowDim.x + windowDim.x / 2; // perspective calculations
+				float py = -translation.z / fovRatY * windowDim.y + windowDim.y / 2;
+				float w = windowDim.x / fovRatY;
 
-		drawData[static_cast<int>(transform.translation.y * 10)].emplaceBack(DrawData { SDL_FRect { rect.rect }, SDL_FRect{
-			(px - w / 2) * windowScale,
-			(py - w / 2) * windowScale,
-			w * windowScale, 
-			w * windowScale
-		} });
+				drawData[static_cast<int>(translation.y * 10)].emplaceBack(DrawData { SDL_FRect { rect.rect }, SDL_FRect{
+					(px - w / 2),
+					(py - w / 2),
+					w,
+					w,
+				} });
+			}
 	};
 
 
@@ -145,8 +154,18 @@ int main(int argc, char** args) {
 					running = false;
 
 					break;
+				} else if (e.type == SDL_EVENT_WINDOW_RESIZED) {
+					SDL_GetWindowSize(window, &windowDim.x, &windowDim.y);
 				} else if (e.type == SDL_EVENT_KEY_DOWN) {
-					std::printf("FPS: %.5f\n", fps);
+					if (e.key.scancode == SDL_SCANCODE_W) {
+						camTransform.get().translation().z += 1;
+					} else if (e.key.scancode == SDL_SCANCODE_S) {
+						camTransform.get().translation().z -= 1;
+					} else if (e.key.scancode == SDL_SCANCODE_A) {
+						camTransform.get().translation().x += 1;
+					} else if (e.key.scancode == SDL_SCANCODE_D) {
+						camTransform.get().translation().x -= 1;
+					}
 				}
 			}
 
