bbd9c68cdaa41ac80ddadd7aa106f44801028f33
Zhile Zhu <zhuzhile08@gmail.com>
2024-09-01 18:05:14 +0200

    Add more useful SDL handle constructors

--- Engine/include/Common/RAIIContainers.h
+++ Engine/include/Common/RAIIContainers.h
@@ -42,17 +42,42 @@ public:
 	constexpr RAIIContainer(movable_container container) noexcept : 
 		m_handle(std::exchange(container.m_handle, handle_type { } )) { }
 
-	constexpr RAIIContainer(lsd::StringView title, int x, int y, int flags) 
+	RAIIContainer(lsd::StringView title, int x, int y, int flags) 
 		requires(std::is_same_v<handle_type, SDL_Window*>) {
 		if (!m_handle) {
 			m_handle = SDL_CreateWindow(title.data(), x, y, flags);
 		}
 	}
 
+	RAIIContainer(SDL_Window* window, lsd::StringView name = { })
+		requires (std::is_same_v<handle_type, SDL_Renderer*>) {
+		if (!m_handle) {
+			m_handle = SDL_CreateRenderer(window, name.data());
+		}
+	}
+
+	RAIIContainer(SDL_Renderer* renderer, SDL_PixelFormat format, SDL_TextureAccess access, int w, int h)
+		requires (std::is_same_v<handle_type, SDL_Texture*>) {
+		if (!m_handle) {
+			m_handle = SDL_CreateTexture(renderer, format, access, w, h);
+		}
+	}
+	RAIIContainer(SDL_Renderer* renderer, SDL_Surface* surface)
+		requires (std::is_same_v<handle_type, SDL_Texture*>) {
+		if (!m_handle) {
+			m_handle = SDL_CreateTextureFromSurface(renderer, surface);
+		}
+	}
+
+
 	void destroy() {
 		if (m_handle) {
 			if constexpr (std::same_as<handle_type, SDL_Window*>) {
 				SDL_DestroyWindow(m_handle);
+			} else if constexpr (std::same_as<handle_type, SDL_Renderer*>) {
+				SDL_DestroyRenderer(m_handle);
+			} else if constexpr (std::same_as<handle_type, SDL_Texture*>) {
+				SDL_DestroyTexutre(m_handle);
 			}
 		}
 
@@ -62,17 +87,18 @@ public:
 		destroy();
 	}
 
+
 	constexpr RAIIContainer& operator=(movable_container other) noexcept {
 		std::swap(m_handle, other.m_handle);
 		return *this;
 	}
 
-	[[nodiscard]] constexpr handle_type release() noexcept {
-		return std::exchange(m_handle, handle_type { });
-	}
-
 	constexpr void swap(container& other) noexcept {
 		std::swap(m_handle, other.m_handle);
+	}
+
+	[[nodiscard]] constexpr handle_type release() noexcept {
+		return std::exchange(m_handle, handle_type { });
 	}
 
 	constexpr handle_type& get() noexcept { return m_handle; }
@@ -90,6 +116,7 @@ namespace sdl {
 
 using Window = detail::RAIIContainer<SDL_Window*>;
 using Renderer = detail::RAIIContainer<SDL_Renderer*>;
+using Surface = detail::RAIIContainer<SDL_Surface*>;
 using Texture = detail::RAIIContainer<SDL_Texture*>;
 
 }
