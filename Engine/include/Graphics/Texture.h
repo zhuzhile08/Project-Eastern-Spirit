db09c5c1c09167167072fc7415e9db8394042581
Zhile Zhu <zhuzhile08@gmail.com>
2024-09-03 10:05:38 +0200

    Redo texture implementation to allow for multiple render passes

--- Engine/include/Graphics/Texture.h
+++ Engine/include/Graphics/Texture.h
@@ -11,27 +11,41 @@
 
 #pragma once
 
+#include <Common/RAIIContainers.h>
+#include <Common/FileSystem.h>
+
+#include <LSD/String.h>
+#include <LSD/UnorderedSparseMap.h>
+
 #include <SDL3/SDL.h>
 
-#include <Common/RAIIContainers.h>
 #include <glm/glm.hpp>
 
 namespace esengine {
 
 class Texture {
 public:
-	Texture(lsd::StringView path);
+	Texture(lsd::StringView path, std::initializer_list<lsd::StringView> passNames = { });
 
-	[[nodiscard]] SDL_Texture* texture() const noexcept {
-		return m_texture.get();
+	void createForPass(lsd::StringView passName);
+
+	[[nodiscard]] SDL_Texture* texture(lsd::StringView passName = "") const noexcept {
+		return m_textures.at(passName).get();
 	}
 	[[nodiscard]] const glm::ivec3& dimension() const noexcept {
 		return m_dimension;
 	}
+	[[nodiscard]] const lsd::String& path() const noexcept {
+		return m_path;
+	}
 
 private:
-	sdl::Texture m_texture;
+	lsd::UnorderedSparseMap<lsd::String, sdl::Texture> m_textures;
 	glm::ivec3 m_dimension = { }; // z represents the channels
+
+	lsd::String m_path;
+
+	void createForPassBackend(lsd::StringView passName, const File& file);
 };
 
 } // namespace esengine
--- Engine/src/Graphics/Texture.cpp
+++ Engine/src/Graphics/Texture.cpp
@@ -9,16 +9,35 @@
 
 namespace esengine {
 
-Texture::Texture(lsd::StringView path) {
-	auto pixels = stbi_load( path.data(), &m_dimension.x, &m_dimension.y, &m_dimension.z, STBI_rgb_alpha);
-	if (pixels == nullptr) {
-		std::printf("Failed to load sprite sheet!");
-	}
+using namespace lsd::enum_operators;
 
-	if (m_texture = sdl::Texture(globals::renderSystem->, SDL_PIXELFORMAT_RGBA32, SDL_TEXTUREACCESS_STATIC, m_dimension.x, m_dimension.y); m_texture)
-		throw std::runtime_error("Failed to create SDL texture!");
+Texture::Texture(lsd::StringView path, std::initializer_list<lsd::StringView> passNames) : m_path(path) {
+	File f = globals::fileSystem->load(globals::fileSystem->assetsPath() / path.data(), OpenMode::binary | OpenMode::read, false);
 
-	if (SDL_Rect dim{ 0, 0, m_dimension.x, m_dimension.y }; SDL_UpdateTexture(m_texture.get(), &dim, pixels, m_dimension.x * m_dimension.z) == SDL_FALSE)
+	for (const auto& name : passNames) createForPassBackend(name, f);
+}
+
+void Texture::createForPass(lsd::StringView passName) {
+	File f = globals::fileSystem->load(globals::fileSystem->assetsPath() / m_path.data(), OpenMode::binary | OpenMode::read, false);
+
+	createForPassBackend(passName, f);
+}
+
+void Texture::createForPassBackend(lsd::StringView passName, const File& file) {
+	auto pixels = stbi_load_from_file(file.stream(), &m_dimension.x, &m_dimension.y, &m_dimension.z, STBI_rgb_alpha);
+	if (pixels == nullptr) throw std::runtime_error("Failed to load sprite sheet!");
+
+	auto& tx = m_textures.emplace(passName, sdl::Texture(
+		globals::renderSystem->pass(passName).renderer, 
+		SDL_PIXELFORMAT_RGBA32, 
+		SDL_TEXTUREACCESS_STATIC, 
+		m_dimension.x, 
+		m_dimension.y)
+	).first->second;
+
+	if (!tx) throw std::runtime_error("Failed to create SDL texture!");
+
+	if (SDL_Rect dim { 0, 0, m_dimension.x, m_dimension.y }; SDL_UpdateTexture(tx.get(), &dim, pixels, m_dimension.x * m_dimension.z) == SDL_FALSE)
 		throw std::runtime_error("Failed to update SDL texutre!");
 
 	stbi_image_free(pixels);
