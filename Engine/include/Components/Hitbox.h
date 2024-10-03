a1445eb1e35965b56b05b35f0529cc0570f3cb69
Zhile Zhu <zhuzhile08@gmail.com>
2024-10-03 20:45:47 +0200

    Start hurt- and hitbox implementation

--- Engine/include/Components/Hitbox.h
+++ Engine/include/Components/Hitbox.h
@@ -1,9 +1,25 @@
-/**
+/*************************
  * @file Hitbox.h
  * @author Zhile Zhu (zhuzhile08@gmail.com)
  * 
- * @brief
+ * @brief Hitboxes which define a box in which hurtboxes can be detected
  * 
  * @date 2024-10-02
  * @copyright Copyright (c) 2024
- */
+ *************************/
+
+#pragma once
+
+#include <glm/glm.hpp>
+
+namespace esengine {
+
+class Hitbox {
+public:
+	constexpr Hitbox(glm::vec3 dimensions) : m_dimensions(dimensions) { }
+
+private:
+	glm::vec3 m_dimensions;
+};
+
+} // namespace esengine
