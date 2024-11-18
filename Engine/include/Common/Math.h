f92a2ad4e19608f9b20f83b5d4e8fa0f741e5d0c
Zhile Zhu <zhuzhile08@gmail.com>
2024-11-18 14:05:53 +0100

    Add overloads for different types

--- Engine/include/Common/Math.h
+++ Engine/include/Common/Math.h
@@ -12,6 +12,8 @@
 
 #include <glm/glm.hpp>
 
+#include <limits>
+
 namespace esengine {
 
 inline GLM_CONSTEXPR glm::vec2 interpolateLinear(glm::vec2 a, glm::vec2 b, float t) noexcept {
@@ -21,8 +23,22 @@ inline GLM_CONSTEXPR glm::dvec2 interpolateLinear(glm::dvec2 a, glm::dvec2 b, do
 	return a * (1.0f - t) + b * t;
 }
 
-inline GLM_CONSTEXPR glm::vec2 mapToQuad(glm::ivec2 quadPos, glm::ivec2 quadDim, glm::ivec2 pos) noexcept {
-	return glm::vec2((pos.x + quadPos.x) % quadDim.x, (pos.y + quadPos.y) % quadDim.y);
+/*
+inline constexpr float interpolateLinear(float low, float high, float t) noexcept {
+	//return a * (1.0f - t) + b * t;
+}
+inline constexpr float interpolateLinear(double low, double high, double t) noexcept {
+	//return a * (1.0f - t) + b * t;
+}
+*/
+
+inline GLM_CONSTEXPR glm::ivec2 mapToQuad(glm::ivec2 quadPos, glm::ivec2 quadDim, glm::ivec2 pos) noexcept {
+	return glm::ivec2((pos.x + quadPos.x) % quadDim.x, (pos.y + quadPos.y) % quadDim.y);
+}
+
+template <class FloatingPoint> inline constexpr bool fpEqual(FloatingPoint a, FloatingPoint b) noexcept requires std::is_floating_point_v<FloatingPoint> {
+	auto diff = a - b;
+	return (diff < std::numeric_limits<FloatingPoint>::epsilon()) && (-diff < std::numeric_limits<FloatingPoint>::epsilon());
 }
 
 } // namespace esengine
