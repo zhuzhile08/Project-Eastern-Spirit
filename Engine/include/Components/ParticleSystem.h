4721f039df7c7261b4ba7f1f927e3fc018b75a52
Zhile Zhu <zhuzhile08@gmail.com>
2024-11-26 09:40:10 +0100

    Add headers for particle system

---
+++ Engine/include/Components/ParticleSystem.h
@@ -0,0 +1,31 @@
+/*************************
+ * @file ParticleSystem.h
+ * @author Zhile Zhu (zhuzhile08@gmail.com)
+ * 
+ * @brief Particle system implementation
+ * 
+ * @date 2024-11-25
+ * @copyright Copyright (c) 2024
+ **************************/
+
+#pragma once
+
+namespace esengine {
+
+class Particle {
+public:
+
+
+private:
+
+};
+
+class ParticleSystem {
+public:
+
+
+private:
+
+};
+
+} // namespace esengine
