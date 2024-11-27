54fb282cf30624cf9107922923c8a14605aa5cda
Zhile Zhu <zhuzhile08@gmail.com>
2024-11-27 17:04:20 +0100

    Add Tilemap header

---
+++ Engine/include/Components/Tilemap.h
@@ -0,0 +1,19 @@
+/************************
+ * @file Tilemap.h
+ * @author Zhile Zhu (zhuzhile08@gmail.com)
+ * 
+ * @brief Tilemap importer and renderer
+ * 
+ * @date 2024-11-27
+ * @copyright Copyright (c) 2024
+ ************************/
+
+#pragma once
+
+namespace esengine {
+
+class Tilemap {
+
+};
+
+} // namespace esengine
