7cd3d2d257b661f371a5aae532053641f3746529
Zhile Zhu <zhuzhile08@gmail.com>
2024-12-11 18:43:56 +0100

    Add files for future use

--- Engine/include/Components/MapManager.h
+++ Engine/include/Components/MapManager.h
@@ -1,10 +1,10 @@
 /************************
- * @file Tilemap.h
+ * @file MapBuilder.h
  * @author Zhile Zhu (zhuzhile08@gmail.com)
  * 
- * @brief Tilemap importer and renderer
+ * @brief Manager responsible for loading maps and levels
  * 
- * @date 2024-11-27
+ * @date 2024-11-29
  * @copyright Copyright (c) 2024
  ************************/
 
@@ -17,13 +17,14 @@
 
 namespace esengine {
 
-class Tilemap {
+class MapManager {
 public:
-	Tilemap(lsd::StringView path);
+	MapManager(lsd::StringView worldPath);
 
 private:
-	lsd::Vector<lsd::Vector<std::size_t>> m_map;
+	//lsd::Vector<lsd::Vector<>> m_screens;
 
+	glm::uvec2 m_currentScreen;
 	glm::uvec2 m_size;
 };
 
---
+++ Engine/src/Components/Tilemap.cpp
@@ -0,0 +1,9 @@
+#include <Components/Tilemap.h>
+
+namespace esengine {
+
+Tilemap::Tilemap(lsd::StringView path) {
+	
+}
+
+} // namespace esengine
